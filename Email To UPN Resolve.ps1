b3b4f68ce6c5829b0d1850c7c5c8c3695ab6fca7
JustJoeO <132582667+JustJoeO@users.noreply.github.com>
2023-05-04 15:00:31 +0100

    Add files via upload

---
+++ Email To UPN Resolve.ps1
@@ -0,0 +1,12 @@
+﻿$emails = Import-Csv -Path "CHANGEME" | Select-Object -ExpandProperty EmailAddress
+$results = foreach ($email in $emails) {
+    $user = Get-ADUser -Filter {EmailAddress -eq $email} -Properties UserPrincipalName
+    if ($user) {
+        [PSCustomObject]@{
+            EmailAddress = $email
+            UserPrincipalName = $user.UserPrincipalName
+        }
+    }
+}
+$results | Export-Csv -Path "CHANGEME" -NoTypeInformation
+
